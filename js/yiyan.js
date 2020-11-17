a03ee0cdf5ae0c0540abd28759aa5f24bb272196
olig48 <58766652+olig48@users.noreply.github.com>
2020-11-17 18:14:25 +0800

    Update yiyan.js

--- js/yiyan.js
+++ js/yiyan.js
@@ -4,9 +4,9 @@ if (Math.random() < 0.5) {
         return res.json();
       })
       .then(function(data) {
-        var yiyan = document.getElementById('hitokotoOrjinrishici');
         hitokotoOrjinrishici = data.hitokoto + '<br>' +
             '&nbsp;&nbsp;&nbsp;—' + data.from;
+        document.getElementById('hitokotoOrjinrishici').value(hitokotoOrjinrishici);
       })
       .catch(function(err) {
         console.error(err);
@@ -20,11 +20,11 @@ if (Math.random() < 0.5) {
         return res.json();
       })
       .then(function(data) {
-        var gushici = document.getElementById('hitokotoOrjinrishici');
         hitokotoOrjinrishici = data.data.content + '<br>' +
             '&nbsp;&nbsp;&nbsp;' +
             '——' + data.data.dynasty + '·' + data.data.author + '<br>' +
             '《' + data.data.title + '》';
+            document.getElementById('hitokotoOrjinrishici').value(hitokotoOrjinrishici);
       })
       .catch(function(err) {
         console.error(err);
